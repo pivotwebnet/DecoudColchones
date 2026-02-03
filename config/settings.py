10af7e0737739bafc54c606ed50a96fa8e3869e8
lautamendoza <lautamendoza122@gmail.com>
2026-02-03 16:42:13 -0300

    Permitir dominio de Vercel

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
+    "https://decoud-colchones.vercel.app/",
 ]
 
 ROOT_URLCONF = 'config.urls'
